d8631eb8067280a4f1f69ee0d82c607d4a5a6d9b
Invizabel <46697708+Invizabel@users.noreply.github.com>
2022-05-13 20:38:55 -0500

    Add files via upload

--- PyCoin.py
+++ PyCoin.py
@@ -8,13 +8,15 @@ def pycoin(email):
     email = sha256(email.encode("ascii")).hexdigest()
     print("hashed email: " + str(email))
     email = str(email)
+    blocks_mined = 0
 
     while True:
         block += 1
         result = sha256(str(block).encode("ascii")).hexdigest()
 
-        if "0000000000000000000000000000000000000000000000000000000000000000" in result:
-            print(block)
+        if "0000000000000000" in result:
+            blocks_mined += 1
+            print("blocks mined: " + blocks_mined)
 
 email = input("Enter email: ")
 os.system("clear")

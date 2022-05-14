5ffb7da64f4f8c6f855a9885c9797a1cd04b312f
Invizabel <46697708+Invizabel@users.noreply.github.com>
2022-05-13 19:12:58 -0500

    Add files via upload

---
+++ PyCoin.py
@@ -0,0 +1,19 @@
+from hashlib import sha256
+
+
+def pycoin(email):
+    block = 0
+    email = sha256(email.encode("ascii")).hexdigest()
+    print("hashed email: " + str(email))
+    email = str(email)
+
+    while True:
+        block += 1
+
+        result = sha256(str(block).encode("ascii")).hexdigest()
+
+        if "0000000000000000000000000000000000000000000000000000000000000000" in result:
+            print(block)
+
+email = input("Enter email: ")
+pycoin(email)

c3cb1a57a8c6c5b5ffe64536b2c2cd6887c688ed
Invizabel <46697708+Invizabel@users.noreply.github.com>
2022-05-14 19:18:56 -0500

    Add files via upload

--- PyCoin.py
+++ PyCoin.py
@@ -6,18 +6,17 @@ os.system("clear")
 
 def pycoin(email):
     block = 0
-    email = sha256(email.encode("ascii")).hexdigest()
-    print("hashed email: " + str(email))
-    email = str(email)
+    email_hash = sha256(email.encode("ascii")).hexdigest()
+    print("hashed email: " + str(email_hash))
     blocks_mined = 0
 
     while True:
         block += 1
         result = sha256(str(block).encode("ascii")).hexdigest()
 
-        if "00000000" in result:
+        if "000000" in result:
             blocks_mined += 1
-            parameters = {"email": str(email), "block": str(block)}
+            parameters = {"login": str(email), "block": str(block)}
             my_request = requests.get("http://localhost:5000/index", params = parameters)
             print(my_request.text)
 
--- back.py
+++ back.py
@@ -6,21 +6,57 @@ app = Flask(__name__)
 @app.route("/index", methods = ["get"])
 
 def pycoin():
-      email = request.args.get("email")
+      email = open("emails.txt", "a")
+      email.close()
+      
+      login = request.args.get("login")
+      signup = request.args.get("signup")
       block = request.args.get("block")
 
-      if email == "49dab445ccba34f55aa2aa7b31f93edb5d9db7faff1834735013d24f57f2f988":
-            result = sha256(block.encode("ascii")).hexdigest()
+      if signup != "" and login == "":
+            result = sha256(signup.encode("ascii")).hexdigest()
+            my_boolean = False
 
-            if "00000000" in result:
-                  return "hash found"
+            with open("emails.txt", "r") as file:
+                  for line in file:
+                        if str(result + "\n") == str(line):
+                              my_boolean = True
+                              break
 
-            else:
-                  return "not a valid hash"
+            if my_boolean == True:
+                  return "user already registered"
+
+            if  my_boolean == False:
+                  email = open("emails.txt", "a")
+                  email.write(result + "\n")
+                  email.close()
+                  return "registered successfully"
+
+      if login != "":
+            my_boolean = False
+            result = sha256(login.encode("ascii")).hexdigest()
+
+            with open("emails.txt", "r") as file:
+                  for line in file:
+                        if str(result + "\n") == str(line):
+                              print(str(result + "\n"))
+                              print(str(line))
+                              my_boolean = True
+                              break
+
+            if my_boolean == True:
+                  result = sha256(block.encode("ascii")).hexdigest()
+
+                  if "000000" in result:
+                        return "hash found"
+
+                  else:
+                        return "not a valid hash"
+
+            if my_boolean == False:
+                  return "user not found"
 
       else:
-            return "user not found"
-
-      
-
+            return "ERROR 404"
+                        
 app.run()
